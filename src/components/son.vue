3b693bdb52a52339e3a5695a6e1ce0d38fdc95b0
jzx666 <573849746@qq.com>
2019-01-21 11:00:53 +0800

    第一次提交

---
+++ src/components/son.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="son">
+        {{$store.state.count}}
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+<style >
+    .son{
+        width: 100px;
+        height: 100px;
+        background-color: yellow;
+    }
+</style>
